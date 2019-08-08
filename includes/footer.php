975f6cf380f6233998c1f78f49879aaa330d95cd
BuildTools <heinrichquirit@gmail.com>
2019-08-08 23:05:14 +1000

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+    <footer id="footer">
+        <i class="fas fa-copyright"></i> 2019 Copyright Lunardo
+    </footer>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+    $title = "Index";
+    include_once('includes/header.php');
+?>
+    <body>
+        <header>
+            <!-- Insert logo here -->
+        </header>
+        <nav>
+            <ul>
+                <li><a>About Us</a></li>
+                <li><a>Prices</a></li>
+                <li><a>Now Showing</a></li>
+            </ul>
+        </nav>
+        <main>
+            <section id="section-one"></section>
+            <section id="section-two"></section>
+            <section id="section-two"></section>
+        </main>
+    </body>
+<?php include_once('includes/footer.php'); ?>
