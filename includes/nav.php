0a628ae65b2e81fd4c23ce2f0a1bee1a201283f6
BuildTools <heinrichquirit@gmail.com>
2019-08-30 18:04:23 +1000

    Final commit for stage two of assignment

--- includes/nav.php
+++ includes/nav.php
@@ -6,5 +6,9 @@
         <a class="nav-links" href="#pricing" style="color:#24282A;">Prices</a>
         <i class="fas fa-tv" style="color:#24282A;"></i>
         <a class="nav-links" href="#now-showing" style="color:#24282A;">Now Showing</a>
+        <i class="fas fa-video" style="color:#24282A;"></i>
+        <a class="nav-links" href="#synopsis" style="color:#24282A;">Synopsis</a>
+        <i class="fas fa-book" style="color:#24282A;"></i>
+        <a class="nav-links" href="#bookings" style="color:#24282A;">Bookings</a>
     </ul>
 </nav>
--- index.php
+++ index.php
@@ -13,25 +13,44 @@
                 <div class="nav-seperator"></div>
                 <section id="about-us">
                     <a name="about"><h1 color="#FFFFFF">About Us</h1></a>
+                    <br/>
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-6">
+                            <h3>Grand Re-Opening</h3>
                             <p>The Lunardo Cinema has a just made a 
                                 grand come back for its re-opening! After
                                 long and hardworking hours for months on end, we have
                                 extensively improved and renovated almost every
                                 feature that we offer!</p>
+                            <h3>What we do</h3>
+                            <p>
+                                We pride ourselves on our employment experience,
+                                giving many young people the launching pad for
+                                their career with ongoing and development opportunities
+                                in communication, customer service and team leadership.
+                                Nearly all of our Lunardo Cinema Manager are promoted 
+                                from our Crew.
+                                <br/>
+                                Our Crew members learn what they need to do on the job 
+                                and in turn, they become the future leaders of our
+                                company. It's on-the-job training like you have never had before.
+                                <br/>
+                                We know that it's our people that set us aparrt from our
+                                competitors to create and deliver the Experience More promise.
+                                It's the people that make a real difference for our customers
+                                and clients.
+                            </p>
                             </div>
                             <div class="col-lg-6">
                                 <img src="media/about-us-room.jpg" class="img-thumbnail" alt="New Cinema" width=500px height=343px/>
                             </div>
                         </div>
                 </section>
-                <div class="section-seperator" style="text-align: center;">
-                    <p>Seperator 1</p>
-                </div>
+                <div class="section-seperator"></div>
                 <section id="pricing">
                     <a name="pricing"><h1 style="color: #51585E;">Seats Pricing</h1></a>
+                    <br/><br/><br/><br/>
                     <table class="table">
                         <thead>
                             <tr>
@@ -81,10 +100,6 @@
                         </tbody>
                     </table>
                 </section>
-                <div style="position:relative;">
-                    <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
-                    </div>
-                </div>
                 <div class="section-seperator"></div>
                 <section class="now-showing">
                     <a name="now-showing"><h1 style="color: #FFFFFF;">Now Showing</h1></a>
@@ -179,7 +194,7 @@
                 <section id="synopsis">
                     <div class="synopsis-panel">
                         <div class="synopsis-title">
-                            <h2>Avengers: Endgame (M)</h2>
+                            <a name="synopsis"><h2>Avengers: Endgame (M)</h2></a>
                         </div>
                         <div class="synopsis-description">
                             Twenty-three days after Thanos used the<br/>
@@ -195,7 +210,7 @@
                             </video>
                         </div>
                         <div class="synopsis-booking">
-                            <h2>Make a booking</h2>
+                        <h2>Make a booking</h2>
                         </div>
                         <div class="synopsis-booking-buttons">
                             <table>
@@ -212,6 +227,14 @@
                         </div>
                     </div>
                 </section>
+                <div class="section-seperator"></div>
+                <section id="bookings">
+                    <div class="bookings-panel">
+                        <a name="bookings"><h1>Bookings</h1></a>
+                        <br/><br/>
+                        <h2>Under construction</h2>
+                    </div>
+                </div>
             </main>
         </div>
     </body>
